0beb3e6227f58dfc6ada2fed5260b9e75869f258
Alexander-Lysenko <linjay@ya.ru>
2017-01-20 04:42:00 +0200

    Попытка разобраться с десериализацией JSONа

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify, escape, redirect
+import sqlite3
 app = Flask(__name__)
 app.debug = True
 
@@ -28,6 +29,29 @@ def profile():
 @app.route("/edit")
 def edit():
     return render_template('edit.html')
+
+
+@app.route("/api/anime/list")
+def api_anime_list():
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    anime = c.execute('''SELECT title, title_original, strftime('%d.%m.%Y %H:%M:%S', release_date), series_count, description
+    FROM anime_list ORDER BY id ASC LIMIT 50;''').fetchall()
+    anime = [[escape(a), escape(b), c, d, escape(e)] for a, b, c, d, e in anime]
+    return jsonify(anime_list=anime)
+
+
+@app.route('/api/anime/add', methods=['POST'])
+def api_anime_add():
+    anime = request.get_json()
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    c.execute('''INSERT INTO anime_list(title, title_original, release_date, series_count, description)
+VALUES(?, ?, ?, ?, ?);''', (anime['title'], anime['title_original'], anime['release_date'], anime['series_count'],
+                            anime['description']))
+    conn.commit()
+    return 'OK'
+
 
 if __name__ == "__main__":
     app.run()
--- static/app.js
+++ static/app.js
@@ -1,21 +1,35 @@
-var app = angular.module('AnimeApp', [])
-app.controller('Homepage', ['$scope', function ($scope) {
-    $scope.message = "Hello, World!";
+var app = angular.module('AnimeApp', ["ngRoute"])
+    .config( ['$routeProvider', function($routeProvider) {
+    $routeProvider
+    .when('/login', {
+    templateUrl: '/templates/login.html',
+        controller: 'Login'
+    })
+    .when('/signup', {
+    templateUrl: '/templates/signup.html',
+        controller: 'Signup'
+    });
 }]);
-app.controller('Signup', ['$scope', function ($scope, $http) {
+
+app.controller('HomepageController', ['$scope', function ($scope) {
+    $scope.message = "Hello, World!";
+    //Проверка сессии
+}]);
+
+app.controller('SignupController', ['$scope', function ($scope, $http) {
     $scope.response = {};
     $scope.submitForm = function (user, userForm) {
         // check to make sure the form is completely valid
         if (userForm.$valid) {
-            /*$http.post("/signup", userForm).success(function (answer) {
+            /*$http.post("/signup", user).success(function (answer) {
                 $scope.response = answer;
             });*/
             alert(angular.toJson(user))
-        }
+        }//не работает POST запрос
     };
 }]);
 
-app.controller('Login', function ($scope){//, $rootScope, AUTH_EVENTS, AuthService) {
+app.controller('LoginController', function ($scope){//, $rootScope, AUTH_EVENTS, AuthService) {
     $scope.submitForm = function (auth, loginForm) {
          /*AuthService.login(loginForm).then(function () {
          $rootScope.$broadcast(AUTH_EVENTS.loginSuccess);
@@ -24,4 +38,22 @@ app.controller('Login', function ($scope){//, $rootScope, AUTH_EVENTS, AuthServi
          });*/
          alert(angular.toJson(auth))
     };
-})
+});
+
+app.controller('AnimeAddController', function ($scope, $http){
+    $scope.response = {};
+    $scope.submitForm = function (edit, editForm) {
+        if (editForm.$valid) {
+            alert(angular.toJson(edit))
+            $http.post("/api/anime/add", edit).success(function (answer) {
+                $scope.response = answer;
+            });
+        };
+    };
+});
+
+app.controller('AnimeListController', function ($scope, $http){
+    $http.get('/static/countries.json').success(function(data) {
+        $scope.request = data;
+    });
+});
