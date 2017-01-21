7bdea2c049445ae86d85e795d55f0f9196940397
Alexander-Lysenko <linjay@ya.ru>
2017-01-21 07:25:33 +0200

    Частично работают вход и регистрация

--- app.py
+++ app.py
@@ -1,44 +1,57 @@
-from flask import Flask, render_template, request, jsonify, escape, redirect
-import sqlite3
+from flask import Flask, render_template, request, jsonify, escape, redirect, make_response
+import random, sqlite3
 app = Flask(__name__)
 app.debug = True
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/")
 def index():
     return render_template('index.html')
 
 
-@app.route("/signup", methods=['GET', 'POST'])
-def signup():
-    if request.method == 'POST':
-        return request.get_json()
-    return render_template('signup.html')
+@app.route("/api/login", methods=['POST'])
+def api_login():
+    auth = request.get_json()
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    username = c.execute('''SELECT user_name FROM users WHERE user_name = :us;''',
+                         {'us': auth['username']}).fetchone()
+    if username is not None:
+        req = make_response(redirect('/'))
+        req.set_cookie('session:', 'dsdf')
+        return 'OK'
+    else:
+        return 'Fail'
 
 
-@app.route("/login")
-def login():
-    return render_template('login.html')
-
-
-@app.route("/profile", methods=['GET', 'POST'])
-def profile():
-    return render_template('profile.html')
-
-
-@app.route("/edit")
-def edit():
-    return render_template('edit.html')
+@app.route("/api/signup", methods=['POST'])
+def api_signup():
+    userdata = request.get_json()
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    username = c.execute('''SELECT user_name FROM users WHERE user_name = :us;''',
+                         {'us': userdata['user_name']}).fetchone()
+    if username is not None:
+        return 'Fail'
+    else:
+        salt = random.randrange(1, 9999)
+        c = conn.cursor()
+        c.execute('''INSERT INTO users(user_name, nickname, email, password, salt)
+    VALUES(?, ?, ?, ?, ?);''', (userdata['user_name'], userdata['nickname'], userdata['email'], userdata['password'],
+                                salt))
+        conn.commit()
+        conn.close()
+        return 'OK'
 
 
 @app.route("/api/anime/list")
 def api_anime_list():
     conn = sqlite3.connect('database.db')
     c = conn.cursor()
-    anime = c.execute('''SELECT title, title_original, strftime('%d.%m.%Y %H:%M:%S', release_date), series_count, description
+    anime = c.execute('''SELECT  id, title, title_original, series_count, strftime('%d.%m.%Y', release_date), description
     FROM anime_list ORDER BY id ASC LIMIT 50;''').fetchall()
-    anime = [[escape(a), escape(b), c, d, escape(e)] for a, b, c, d, e in anime]
-    return jsonify(anime_list=anime)
+    anime = [[a, escape(b), escape(c), d, e, escape(f)] for a, b, c, d, e, f in anime]
+    return jsonify({'anime_list': anime})
 
 
 @app.route('/api/anime/add', methods=['POST'])
@@ -49,6 +62,15 @@ def api_anime_add():
     c.execute('''INSERT INTO anime_list(title, title_original, release_date, series_count, description)
 VALUES(?, ?, ?, ?, ?);''', (anime['title'], anime['title_original'], anime['release_date'], anime['series_count'],
                             anime['description']))
+    conn.commit()
+    return 'OK'
+
+
+@app.route("/api/anime/rm/<anime_id>", methods=['POST'])
+def api_anime_rm(anime_id):
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    c.execute('''DELETE FROM anime_list WHERE id = ?;''', anime_id)
     conn.commit()
     return 'OK'
 
--- static/app.js
+++ static/app.js
@@ -1,67 +1,109 @@
-var app = angular.module('AnimeApp', ["ngRoute"]);
-app.config(function($routeProvider) {
+var app = angular.module('AnimeApp', [
+    'ngRoute',
+    'ngCookies']);
+app.config(['$locationProvider', function ($locationProvider) {
+    $locationProvider.hashPrefix('');
+}]);
+app.config(function ($routeProvider) {
     $routeProvider
-    .when("/", {
-        templateUrl : "/static/html/login.html",
-        controller : "HomepageController"
-    })
-    .when("/login", {
-        templateUrl : "/static/html/login.html",
-        controller : "LoginController"
-    })
-    .when("/signup", {
-        templateUrl : "signup.htm",
-        controller : "SignupController"
-    })
-    .when("/profile", {
-        templateUrl : "profile.htm",
-        controller : "ProfileController"
-    });
+        .when("/", {
+            templateUrl: "/static/edit.html",
+            controller: "AnimeListController"
+        })
+        .when("/login", {
+            templateUrl: "/static/login.html",
+            controller: "LoginController"
+        })
+        .when("/signup", {
+            templateUrl: "/static/signup.html",
+            controller: "SignupController"
+        })
+        .when("/profile", {
+            templateUrl: "/static/profile.html",
+            controller: "ProfileController"
+        })
+        .otherwise({redirectTo: '/login'});
 });
 
 app.controller('HomepageController', ['$scope', function ($scope) {
     $scope.message = "Hello, World!";
-    //Проверка сессии
 }]);
 
-app.controller('SignupController', ['$scope', function ($scope, $http) {
-    $scope.response = {};
+app.controller('SignupController', function ($scope, $http) {
     $scope.submitForm = function (user, userForm) {
-        // check to make sure the form is completely valid
         if (userForm.$valid) {
-            /*$http.post("/signup", user).success(function (answer) {
-                $scope.response = answer;
-            });*/
-            alert(angular.toJson(user))
+            $http.post("/api/signup", user).then(function (response) {
+                if (response.data == 'OK') {
+                    $scope.message = 'Вы успешно зарегистрировались';
+                }
+                else {
+                    $scope.error = 'Ошибка';
+                    console.log(response);
+                }
+            })
         }
     };
-}]);
-
-app.controller('LoginController', function ($scope){//, $rootScope, AUTH_EVENTS, AuthService) {
-    $scope.submitForm = function (auth, loginForm) {
-         /*AuthService.login(loginForm).then(function () {
-         $rootScope.$broadcast(AUTH_EVENTS.loginSuccess);
-         }, function () {
-         $rootScope.$broadcast(AUTH_EVENTS.loginFailed);
-         });*/
-         alert(angular.toJson(auth))
-    };
 });
 
-app.controller('AnimeAddController', function ($scope, $http){
-    $scope.response = {};
+app.controller('LoginController',
+    ['$scope', '$rootScope', '$location', 'AuthenticationService',
+        function ($scope, $rootScope, $location, AuthenticationService) {
+            // reset login status
+            AuthenticationService.ClearCredentials();
+
+            $scope.login = function () {
+                $scope.dataLoading = true;
+                AuthenticationService.Login($scope.username, $scope.password, function (response) {
+                    if (response.data == 'OK') {
+                        AuthenticationService.SetCredentials($scope.username, $scope.password);
+                        $location.path('/');
+                    } else {
+                        $scope.error = response.message;
+                        $scope.dataLoading = false;
+                    }
+                });
+            };
+        }]);
+
+app.controller('AnimeListController', function ($scope, $http) {
+    $scope.requestData = function () {
+        $http.get('/api/anime/list').then(function (request) {
+            $scope.anime_list = request.data.anime_list;
+        });
+    };
     $scope.submitForm = function (edit, editForm) {
         if (editForm.$valid) {
-            alert(angular.toJson(edit))
-            $http.post("/api/anime/add", edit).success(function (answer) {
-                $scope.response = answer;
-            });
-        };
+            $http.post("/api/anime/add", edit).then(requestData());
+        }
+    };
+    $scope.removeAnime = function (number) {
+        alert(number);
+        $http.post('/api/anime/rm/' + number).then(requestData())
     };
 });
 
-app.controller('AnimeListController', function ($scope, $http){
-    $http.get('/static/countries.json').success(function(data) {
-        $scope.request = data;
-    });
-});
+app.controller('ProfileController', ['$scope', function ($scope) {
+    $scope.message = "Hello, World!";
+}]);
+
+app.run(['$rootScope', '$location', '$cookieStore', '$http',
+    function ($rootScope, $location, $cookieStore, $http) {
+        // keep user logged in after page refresh
+        $rootScope.globals = $cookieStore.get('globals') || {};
+        if ($rootScope.globals.currentUser) {
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata;
+            // jshint ignore:line
+        }
+
+        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+            // redirect to login page if not logged in
+            /*var restrictedPage = $.inArray($location.path(), ['/login', '/signup']) === -1;
+             var loggedIn = $rootScope.globals.currentUser;
+             if (restrictedPage && !loggedIn) {
+             $location.path('/login');
+             }*/
+            if (($location.path() !== '/login' && $location.path() !== '/signup') && !$rootScope.globals.currentUser) {
+                $location.path('/login');
+            }
+        });
+    }]);
